b794e8943956d64da4b0c399f75a9d92eb3a8b14
tiago <osbentoalves@gmail.com>
2018-07-25 20:02:17 -0300

    teste do push de casa

---
+++ SGEDAO/Entity/ClienteMap.cs
@@ -0,0 +1,18 @@
+﻿using SGEDOMINIO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGEDAO.Entity
+{
+  public  class ClienteMap : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteMap()
+        {
+
+        }
+    }
+}
